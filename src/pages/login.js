bf708c9a94d826b71cb0a4305039f1bec490efac
Abid Ullah <abidullah.se@gmail.com>
2024-03-27 15:59:43 +0500

    intial setup done

---
+++ src/pages/login.js
@@ -0,0 +1,12 @@
+import styles from './login.module.scss'
+const Login = () => {
+    return (
+        <>
+            <h2 className={styles.heading}>Login</h2>
+        </>
+    )
+
+
+}
+
+export default Login;
